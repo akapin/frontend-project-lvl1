d1863acec75982cd6d038f903e39f5f7e2270eaf
Alex Kapin <alexander.kapin@outlook.com>
2019-08-02 11:06:25 +0300

    refactor gcd game module

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,29 +2,20 @@ import { playGame, getRandomInteger } from '..';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const getTwoRandomNumbersString = (minNumber, maxNumber) => {
-  const firstNumber = getRandomInteger(minNumber, maxNumber);
-  const secondNumber = getRandomInteger(minNumber, maxNumber);
-  return `${firstNumber} ${secondNumber}`;
-};
+const minNumber = 0;
+const maxNumber = 100;
 
-const getGDC = (numbersString) => {
-  const numbersArray = numbersString.split(' ');
-  const number1 = Number(numbersArray[0]);
-  const number2 = Number(numbersArray[1]);
-  const iter = (a, b) => {
-    if (!b) return a;
-    return iter(b, a % b);
-  };
-  return String(iter(number1, number2));
+const getGCD = (a, b) => {
+  if (!b) return a;
+  return getGCD(b, a % b);
 };
 
 const getQuestionAnswerPair = () => {
-  const minNumber = 0;
-  const maxNumber = 100;
-  const twoRandomNumbersString = getTwoRandomNumbersString(minNumber, maxNumber);
-  const gdc = getGDC(twoRandomNumbersString);
-  return { question: twoRandomNumbersString, correctAnswer: gdc };
+  const firstNumber = getRandomInteger(minNumber, maxNumber);
+  const secondNumber = getRandomInteger(minNumber, maxNumber);
+  const question = `${firstNumber} ${secondNumber}`;
+  const correctAnswer = String(getGCD(firstNumber, secondNumber));
+  return { question, correctAnswer };
 };
 
 export default () => playGame(gameDescription, getQuestionAnswerPair);
