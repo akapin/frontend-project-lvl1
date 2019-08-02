88763219978cfb7ff06a6e4340240ed3ac59ee66
Alex Kapin <alexander.kapin@outlook.com>
2019-08-02 10:55:36 +0300

    refactor calc game module

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,43 +4,33 @@ const gameDescription = 'What is the result of the expression?';
 
 const symbolMap = '+-*';
 const startIndex = 0;
-const expressionMinNumber = 0;
-const expressionMaxNumber = 100;
+const minNumber = 0;
+const maxNumber = 100;
 
-const getMathExpressionString = (minNumber, maxNumber) => {
+const getQuestionAnswerPair = () => {
   const firstOperand = getRandomInteger(minNumber, maxNumber);
   const secondOperand = getRandomInteger(minNumber, maxNumber);
   const randomIndex = getRandomInteger(startIndex, symbolMap.length);
   const operator = symbolMap.charAt(randomIndex);
-  return `${firstOperand} ${operator} ${secondOperand}`;
-};
+  const expressionString = `${firstOperand} ${operator} ${secondOperand}`;
 
-const getExpressionResult = (expression) => {
-  const expressionArray = expression.split(' ');
-  const firstOperand = Number(expressionArray[0]);
-  const secondOperand = Number(expressionArray[2]);
-  const operator = expressionArray[1];
-  let result;
+  let expressionResult;
+
   switch (operator) {
     case '+':
-      result = firstOperand + secondOperand;
+      expressionResult = firstOperand + secondOperand;
       break;
     case '-':
-      result = firstOperand - secondOperand;
+      expressionResult = firstOperand - secondOperand;
       break;
     case '*':
-      result = firstOperand * secondOperand;
+      expressionResult = firstOperand * secondOperand;
       break;
     default:
       break;
   }
-  return String(result);
-};
 
-const getQuestionAnswerPair = () => {
-  const expressionString = getMathExpressionString(expressionMinNumber, expressionMaxNumber);
-  const expressionResult = getExpressionResult(expressionString);
-  return { question: expressionString, correctAnswer: expressionResult };
+  return { question: expressionString, correctAnswer: String(expressionResult) };
 };
 
 export default () => playGame(gameDescription, getQuestionAnswerPair);
