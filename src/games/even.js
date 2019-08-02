56f0cfe3478e47a20bfe92ca6095f51c4e57022e
Alex Kapin <alexander.kapin@outlook.com>
2019-08-02 10:47:19 +0300

    move number constants from getQuestionAnswerPair function

--- src/games/even.js
+++ src/games/even.js
@@ -4,9 +4,10 @@ const gameDescription = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = number => number % 2 === 0;
 
+const minNumber = 0;
+const maxNumber = 100;
+
 const getQuestionAnswerPair = () => {
-  const minNumber = 0;
-  const maxNumber = 100;
   const question = getRandomInteger(minNumber, maxNumber);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return { question, correctAnswer };
