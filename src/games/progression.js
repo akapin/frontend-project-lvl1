8667bbc5dbcb5b889ff31b9f4e28ae00394bfcc8
Alex Kapin <alexander.kapin@outlook.com>
2019-08-02 11:30:27 +0300

    refactor progression game module

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,15 @@ import { playGame, getRandomInteger } from '..';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const getProgressionString = (steps, startNumber, progressionStep, hiddenElement) => {
+const steps = 10;
+const minStartNumber = 0;
+const maxStartNumber = 10;
+const minStep = 1;
+const maxStep = 10;
+const minSkipStep = 1;
+const maxSkipStep = 10;
+
+const getProgressionString = (startNumber, progressionStep, hiddenElement) => {
   let progressionString = '';
   let currentNumber = startNumber;
   for (let i = 1; i <= steps; i += 1) {
@@ -16,22 +24,13 @@ const getProgressionString = (steps, startNumber, progressionStep, hiddenElement
   return progressionString.trim();
 };
 
-const getMissingNumber = (startNum, step, hiddenElement) => startNum + step * (hiddenElement - 1);
-
 const getQuestionAnswerPair = () => {
-  const steps = 10;
-  const minStartNumber = 0;
-  const maxStartNumber = 10;
   const startNumber = getRandomInteger(minStartNumber, maxStartNumber);
-  const minStep = 1;
-  const maxStep = 10;
-  const step = getRandomInteger(minStep, maxStep);
-  const minSkipStep = 1;
-  const maxSkipStep = 10;
+  const progressionStep = getRandomInteger(minStep, maxStep);
   const hiddenElement = getRandomInteger(minSkipStep, maxSkipStep);
-  const progressionString = getProgressionString(steps, startNumber, step, hiddenElement);
-  const missingNumber = getMissingNumber(startNumber, step, hiddenElement);
-  return { question: progressionString, correctAnswer: String(missingNumber) };
+  const question = getProgressionString(startNumber, progressionStep, hiddenElement);
+  const correctAnswer = String(startNumber + progressionStep * (hiddenElement - 1));
+  return { question, correctAnswer };
 };
 
 export default () => playGame(gameDescription, getQuestionAnswerPair);
