418afdd8787d5486756f6408ccaafdeb9aabbcb8
Alex Kapin <alexander.kapin@outlook.com>
2019-08-05 13:57:25 +0300

    split wrong answer message

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ export default (gameDescription, getQuestionAnswerPair) => {
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
